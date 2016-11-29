dea9bffaea0d1331c63740938ba0674b1e8119b4
Dima Diachenko <dimatrump@gmail.com>
2016-11-29 15:48:59 +0200

    update 29.11

--- students/views.py
+++ students/views.py
@@ -1,10 +1,29 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for Students
 
 def students_list(request):
-	return render(request, 'students/students_list.html', {})
+	students = (
+		{'id': 1,
+		'first_name': u'Дмитро',
+		'last_name': u'Дяченко',
+		'ticket': 21,
+		'image': 'img/beavis-2.png'},
+		{'id': 2,
+		'first_name': u'Анатолій',
+		'last_name': u'Кустливий',
+		'ticket': 27,
+		'image': 'img/bart.jpg'},
+		{'id': 3,
+		'first_name': u'Микола',
+		'last_name': u'Кустливий',
+		'ticket': 28,
+		'image': 'img/butthead.jpg'},
+		)
+	return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
 	return HttpResponse('<h1>Student Add Form</h1>')
