539e1758dea2b3f0b7004f394aae21e751824c34
Dima Diachenko <dimatrump@gmail.com>
2016-12-11 14:35:58 +0200

    created tables groups, students

--- students/views/students.py
+++ students/views/students.py
@@ -2,28 +2,10 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..models import Student
 
 def students_list(request):
-	students = (
-		{'id': 1,
-		'first_name': u'Дмитро',
-		'last_name': u'Дяченко',
-		'group': u'КСД-21',
-		'ticket': 21,
-		'image': 'img/beavis-2.png'},
-		{'id': 2,
-		'first_name': u'Анатолій',
-		'last_name': u'Кустливий',
-		'group': u'КСД-22',
-		'ticket': 27,
-		'image': 'img/bart.jpg'},
-		{'id': 3,
-		'first_name': u'Микола',
-		'last_name': u'Кустливий',
-		'group': u'КСД-23',
-		'ticket': 28,
-		'image': 'img/butthead.jpg'},
-		)
+	students = Student.objects.all()
 	return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
